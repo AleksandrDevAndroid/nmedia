7a4e353a7f530929598a37476e9de0aecd4d07e6
Aleks_Android <aleksperdev@gmail.com>
2025-10-21 17:38:28 +0300

    origin

---
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostActivity.kt
@@ -0,0 +1,14 @@
+package ru.netology.nmedia.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ru.netology.nmedia.DTO.Post
+import ru.netology.nmedia.R
+
+object NewPostResultContract : ActivityResultContracts<Unit,String?>() {
+
+    }
